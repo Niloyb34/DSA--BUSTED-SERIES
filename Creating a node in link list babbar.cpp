0ba20418f33fef5daf3cf2813ec3c7a42f03c109
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-04-06 20:33:13 +0600

    Create Creating a node in link list babbar.cpp

---
+++ Creating a node in link list babbar.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node* next;///Node typer akta pointer ja kina next node ke point korbe
+};
+int main()
+{
+   Node* node1=new Node();
+   cout<<node1->data<<endl;///mane node 1 er data dekabe
+   cout<<node1->next<<endl;///mane node 1 er address dekabe
+}
+//////////  j   u    s   t    akta node create korlam jar jnno amra data and element gulake dukie dilam
+///initially eigular  modde muloto kono node er data nei and address ba link e next node er kono value o nei
