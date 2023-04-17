ba57a7d719136f613977a8d8fe9465efc9e60060
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-04-17 19:30:13 +0600

    Update Creating node using class  bababr.cpp

--- Creating node using class  bababr.cpp
+++ Creating node using class  bababr.cpp
@@ -1,3 +1,4 @@
+////////////////////////////////////NUMBER 1 CODE TA DEKO WITH USING THIS POINTER/////
 #include<bits/stdc++.h>
 using namespace std;
 class Node
@@ -21,3 +22,23 @@ int main()
    cout<<node1->data<<endl;
    cout<<node1->next<<endl;
 }
+//////////////////////////////  NUMBER 2 CODE DEKO ////////////////////////////////////
+//////////////////////////////  OR WITHOUT USING THIS POINTER//////////////////////////
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node* next;///Node typer akta pointer ja kina next node ke point korbe
+};
+int main()
+{
+   Node* node1=new Node();
+   node1->data=10;
+   node1->next=NULL;
+   cout<<node1->data<<endl;///mane node 1 er data dekabe
+   cout<<node1->next<<endl;///mane node 1 er address dekabe
+}
+//////////  j   u    s   t    akta node create korlam jar jnno amra data and element gulake dukie dilam
+///initially eigular  modde muloto kono node er data nei and address ba link e next node er kono value o nei
